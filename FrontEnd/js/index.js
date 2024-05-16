148eb25715457483655549085dc1590b9df3dcd6
Kevin Lavier <kevinlavier@orange.fr>
2024-05-16 21:17:36 +0200

    feat: Ajoute les filtres projets, la structure HTML et le style css qui va avec

--- FrontEnd/js/index.js
+++ FrontEnd/js/index.js
@@ -1,15 +1,44 @@
 //......API GET WORKS
 
+let works = [];
+let categories = [];
+
+const fetchWorks = fetch("http://localhost:5678/api/works");
+const fetchCategories = fetch("http://localhost:5678/api/categories");
+
+// Load DOM before execute the function getWorks and addFilterEventListener
+
+document.addEventListener("DOMContentLoaded", () => {
+  getWorks();
+  getCategories();
+  addFilterEventListeners();
+});
+
 // Fetch GET api/works
 
 async function getWorks() {
   try {
-    const response = await fetch("http://localhost:5678/api/works");
+    const response = await fetchWorks;
     if (response.ok) {
-      const works = await response.json();
-      displayWorks(works); // Function that manipulate DOM
+      works = await response.json();
+      displayWorks(works);
     } else {
       throw new Error("Impossible de contacter le serveur");
+    }
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+// Fetch GET api/categories
+
+async function getCategories() {
+  try {
+    const response = await fetchCategories;
+    if (response.ok) {
+      categories = await response.json();
+    } else {
+      throw new Error("Impossible de contacter le serveur pour les catégories");
     }
   } catch (error) {
     console.error(error);
@@ -37,8 +66,33 @@ function displayWorks(works) {
   });
 }
 
-// Load DOM before execute the function getWorks
+//......FILTERS
 
-document.addEventListener("DOMContentLoaded", () => {
-  getWorks();
-});
+// addEventListener on button that have data-id
+
+function addFilterEventListeners() {
+  const filtersSection = document.querySelector("#filters");
+  const buttons = filtersSection.querySelectorAll("button");
+
+  buttons.forEach((button) => {
+    button.addEventListener("click", (event) => {
+      const categoryId = event.target.getAttribute("data-id");
+
+      buttons.forEach((btn) => btn.classList.remove("active"));
+
+      event.target.classList.add("active");
+
+      filterWorks(categoryId);
+    });
+  });
+}
+
+// Filters then display projects per category
+
+function filterWorks(categoryId) {
+  const filteredWorks =
+    categoryId === "all"
+      ? works
+      : works.filter((work) => work.categoryId == categoryId);
+  displayWorks(filteredWorks);
+}
